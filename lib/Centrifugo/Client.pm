286d31ce5fe26214e4841bcd967b040662b799bd
Orabig <benoit.chauvet@gmail.com>
2017-08-12 13:13:03 +0200

    Refactor

--- lib/Centrifugo/Client.pm
+++ lib/Centrifugo/Client.pm
@@ -65,6 +65,7 @@ or
 	   refresh_period => 5,      # Check frequency for max_alive_period (default 10s)
 	   retry => 0.5 ,            # interval (in ms) between reconnect attempts which value grows exponentially (default 1.0)
 	   max_retry => 30,          # upper interval value limit when reconnecting. (default 30)
+	   resubscribe => 'true',    # automatic resubscribing on subscriptions (default: 'true')
 	   recover => 'true',        # Recovers the lost messages after a reconnection (default: 'false')
 	   ws_params => {            # These parameters are passed to AnyEvent::WebSocket::Client->new(...)
 			 ssl_no_verify => 'true',
@@ -79,14 +80,18 @@ sub new {
 	my $this = {};
 	bless($this, $class);
 	$this->{WS_URL} = $ws_url;
-	$this->{DEBUG} = $params{debug} && uc($params{debug})ne'FALSE';
-	$this->{AUTH_URL} = $params{authEndpoint} || "/centrifuge/auth/";
-	$this->{WEBSOCKET} = AnyEvent::WebSocket::Client -> new( %{$params{ws_params}} );
-	$this->{MAX_ALIVE} = $params{max_alive_period} || 0;
-	$this->{REFRESH} = $params{refresh_period} || 10;
-	$this->{RETRY} = $params{retry} || 1;
-	$this->{MAX_RETRY} = $params{max_retry} || 30;
-	$this->{RECOVER} = $params{recover};
+	$this->{DEBUG} = $params{debug} && $params{debug}!~/^(0|false|no)$/i; delete $params{debug};
+	$this->{AUTH_URL} = delete $params{authEndpoint} || "/centrifuge/auth/";
+	$this->{WEBSOCKET} = AnyEvent::WebSocket::Client -> new( %{$params{ws_params}} ); delete $params{ws_params};
+	$this->{MAX_ALIVE} = delete $params{max_alive_period} || 0;
+	$this->{REFRESH} = delete $params{refresh_period} || 10;
+	$this->{RETRY} = delete $params{retry} || 1;
+	$this->{MAX_RETRY} = delete $params{max_retry} || 30;
+	$this->{RESUBSCRIBE} = $params{resubscribe} || $params{resubscribe}!~/^(0|false|no)$/i; delete $params{resubscribe};
+	$this->{RECOVER} = $params{recover} && $params{recover}!~/^(0|false|no)$/i; delete $params{recover};
+	$this->{_first_connection} = 'TRUE'; # This indicator is useful for recover algorithm
+	croak "Centrifugo::Client : Unknown parameter : ".join',',keys %params if %params;
+	print "resub=$this->{RESUBSCRIBE}";
 	return $this;
 }
 
@@ -114,8 +119,8 @@ sub connect {
 	# Fix parameters sent to Centrifugo
 	$PARAMS{timestamp}="$PARAMS{timestamp}" if $PARAMS{timestamp}; # This MUST be a string
 	# Save the Centrifugo connection parameters
-	$this->{CNX_UID} = delete $PARAMS{uid} || _generate_random_id();
-	$this->{CNX_PARAMS} = \%PARAMS;
+	$this->{_cnx_uid} = delete $PARAMS{uid} || _generate_random_id();
+	$this->{_cnx_params} = \%PARAMS;
 	
 	# Connects to Websocket
 	$this->_reset_reconnect_sequence();
@@ -155,23 +160,54 @@ sub _on_ws_connect {
 	# Then, connects to Centrifugo
 	$this->_send_message( {
 		method => 'connect',
-		UID => $this->{CNX_UID},
-		params => $this->{CNX_PARAMS}
+		UID => $this->{_cnx_uid},
+		params => $this->{_cnx_params}
 	} );
 }
 
 # This function is called when client is connected to Centrifugo
 sub _on_connect {
 	my ($this, $body) = @_;
-	print STDERR "Centrifugo::Client : Connected to Centrifugo : $body\n" if $this->{DEBUG};	
+	print STDERR "Centrifugo::Client : Connected to Centrifugo : ".encode_json $body if $this->{DEBUG};	
 	# on Connect, the client_id must be read (if available)
 	if ($body && ref($body) eq 'HASH' && $body->{client}) {
 		$this->{CLIENT_ID} = $body->{client};
 		print STDERR "Centrifugo::Client : CLIENT_ID=".$this->{CLIENT_ID}."\n" if $this->{DEBUG};
 	}
 	$this->_init_keep_alive_timer() if $this->{MAX_ALIVE};
-	print STDERR "Centrifugo::Client : _reset_reconnect_sequence( _on_connect )";
 	$this->_reset_reconnect_sequence();
+	$this->_resubscribe() if $this->{RESUBSCRIBE};
+	delete $this->{_first_connection}; # Next connection should use resubscribe and recover is any
+}
+
+# This function is called when client is connected to Centrifugo
+sub _on_subscribe {
+	my ($this, $body) = @_;
+	my $channel = $body->{channel};
+	print STDERR "Centrifugo::Client : Subscribed to $channel : ".encode_json $body if $this->{DEBUG};
+	# Keeps track of channels
+	$this->{channels}->{ $channel } = $body;
+}
+
+# This function is called when client is connected to Centrifugo
+sub _on_unsubscribe {
+	my ($this, $body) = @_;
+	my $channel = $body->{channel};
+	print STDERR "Centrifugo::Client : Unsubscribed from $body->{channel} : ".encode_json $body if $this->{DEBUG};
+	# Keeps track of channels
+	delete $this->{channels}->{ $channel };
+}
+
+# This function automatically reconnects to channels
+sub _resubscribe {
+	my ($this) = @_;
+	foreach my $channel (keys %{$this->{channels}}) {
+		print STDERR "Centrifugo::Client : Resubscribe to $channel" if $this->{DEBUG};
+		$this->subscribe( 
+			channel => $channel,
+			client => $this->client_id()
+		);
+	}
 }
 
 # This function is called when the connection with server is lost
@@ -179,7 +215,7 @@ sub _on_close {
 	my ($this, $message) = @_;
 	print STDERR "Centrifugo::Client : Connection closed (reason=$message)\n" if $this->{DEBUG};
 	$this->{ON}->{'ws_closed'}->($message) if $this->{ON}->{'ws_closed'};
-	undef $this->{alive_handler};
+	undef $this->{_alive_handler};
 	undef $this->{WSHANDLE};
 	undef $this->{CLIENT_ID};
 	$this->_reconnect();
@@ -196,19 +232,20 @@ sub _on_error {
 sub _on_message {
 	my ($this, $message) = @_;
 	print STDERR "Centrifugo::Client : R< WS : $message->{body}\n" if $this->{DEBUG};
-	$this->{last_alive_message} = time();
-	my $fullbody = decode_json($message->{body});
-	# Handle a body containing {response}
+	$this->{_last_alive_message} = time();
+	my $fullbody = decode_json($message->{body}); # The body of websocket message
+	# Handle a body containing {response} : converts into a singleton
 	if (ref($fullbody) eq 'HASH') {
 		$fullbody = [ $fullbody ];
 	}
-	# Handle a body containing [{response},{response}...]
+	# Handle the body which is now an array of response
 	foreach my $info (@$fullbody) {
 		my $uid = $info->{uid};
 		my $method = $info->{method};
-		my $error = $info->{error};
-		my $body = $info->{body}; # Not the same 'body' as above
+		my $body = $info->{body}; # The body of Centrifugo message
 		$this->_on_connect( $body ) if $method eq 'connect';
+		$this->_on_subscribe( $body ) if $method eq 'subscribe';
+		$this->_on_unsubscribe( $body ) if $method eq 'unsubscribe';
 
 		# Call the callback of the method
 		my $sub = $this->{ON}->{$method};
@@ -225,8 +262,8 @@ sub _on_message {
 # Inits the Fibonacci sequence for reconnection retries
 sub _reset_reconnect_sequence {
 	my ($this) = @_;
-	$this->{last_retry} = 0;
-	$this->{next_retry} = $this->{RETRY};
+	$this->{_last_retry} = 0;
+	$this->{_next_retry} = $this->{RETRY};
 }
 
 # Reconnects to the server after a loss of connection
@@ -234,14 +271,14 @@ sub _reset_reconnect_sequence {
 # fibonacci sequence to get interval between reconnect attempts which value grows exponentially. (why not ?)
 sub _reconnect {
 	my ($this) = @_;
-	my $retry_after = $this->{next_retry};
-	$retry_after = $this->{MAX_RETRY} if $retry_after > $this->{MAX_RETRY};
+	my $retry_after = $this->{_next_retry} > $this->{MAX_RETRY} ? $this->{MAX_RETRY} : $this->{_next_retry};
+	$retry_after = int($retry_after) if $retry_after > 3;
 	print STDERR "Centrifugo::Client : will reconnect after $retry_after s.\n" if $this->{DEBUG};
 	$this->{reconnect_handler} = AnyEvent->timer(
 		after => $retry_after,
 		cb => sub {
-			$this->{next_retry} += $this->{last_retry};
-			$this->{last_retry} = $retry_after;
+			$this->{_next_retry} += $this->{_last_retry};
+			$this->{_last_retry} = $retry_after;
 			$this->_connect();
 		}
 	);
@@ -250,11 +287,11 @@ sub _reconnect {
 # Creates the timer to send periodic ping
 sub _init_keep_alive_timer {
 	my ($this) = @_;
-	$this->{alive_handler} = AnyEvent->timer(
+	$this->{_alive_handler} = AnyEvent->timer(
 		after => $this->{REFRESH},
 		interval => $this->{REFRESH},
 		cb => sub {
-			my $late = time() - $this->{last_alive_message};
+			my $late = time() - $this->{_last_alive_message};
 			if ($late > $this->{MAX_ALIVE}) {
 				print STDERR "Sending ping (${late}s without message)\n" if $this->{DEBUG};
 				$this->ping();
@@ -311,9 +348,9 @@ sub disconnect {
 
 =head1 FUNCTION subscribe - allows to subscribe on channel after client successfully connected.
 
-$client->subscribe( channel => $channel, [ client => $clientId  ,] [ uid => $uid ,] );
+$client->subscribe( channel => $channel, [ uid => $uid ,] );
 
-If the channel is private (starts with a '$'), then a request to $this->{AUTH_URL} is done automatically to get the channel key. In that case, the 'client' parameter is mandatory.
+If the channel is private (starts with a '$'), then a request to $this->{AUTH_URL} is done automatically to get the channel key.
 
 This function returns the UID used to send the command to the server. (a random string if none is provided)
 
@@ -321,18 +358,18 @@ This function returns the UID used to send the command to the server. (a random 
 
 sub subscribe {
 	my ($this, %PARAMS) = @_;
-	my $channel = $PARAMS{channel};
+	my $channel = $PARAMS{channel}; # TODO : Handle list of channels (watch out for return values)
 	return _channel_command($this,'subscribe',%PARAMS) unless $channel=~/^\$/;
 	# If the channel is private, then an API-call to /centrifuge/auth/ must be done
-	croak "'client' parameter is mandatory to subscribe to private channels in Centrifugo::Client->subscribe(...)" unless $PARAMS{client};
+	croak "Can't subscribe to private channels : Client is not connected in Centrifugo::Client->subscribe(...)" unless $this->client_id();
 
 	# Request a channel key
 	my $data = encode_json {
-		client => $PARAMS{client},
+		client => $this->client_id(),
 		channels => [ $channel ]
 	};
 	my $URL = $this->{AUTH_URL};
-	http_post $URL, $data, 
+	http_post $URL, $data,
 		headers => {
 			contentType => "application/json"
 		},
@@ -342,7 +379,7 @@ sub subscribe {
 		  my $result = decode_json $data;
 		  my $key = $result->{$channel}->{sign};
 		  $PARAMS{sign} = $key;
-		  # The request is now complete : {channel:"...", client:"...", sign:"..."}
+		  # The request is now complete : {channels: ["...",...], client:"...", sign:"..."}
 		  return _channel_command($this,'subscribe',%PARAMS);
 	   };
 }
